f14edb98518325dcf112c4cad95a42a8d92c0c5a
oregetonii <proffzion30@gmail.com>
2023-10-15 18:39:39 +0000

    Handles %%, %c, and %s

--- 0-printf.c
+++ 0-printf.c
@@ -10,19 +10,21 @@
 int _printf(const char *format, ...)
 {
 	int print_count;
+	int count;
 	va_list print_list;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(print_list, format);
+
 	print_count = 0;
 	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			print_count++;
+			count = write(1, format, 1);
+			print_count += count;
 		}
 
 		else
@@ -30,14 +32,14 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '%')
 			{
-				write(1,format, 1);
-				print_count++;
+				count = write(1,format, 1);
+				print_count += count;
 			}
 			if (*format == 'c')
 			{
 				char ch = va_arg(print_list, int);
-				write(1, &ch, 1);
-				print_count++;
+				count = write(1, &ch, 1);
+				print_count += count;
 			}
 			if (*format == 's')
 			{
@@ -46,12 +48,15 @@ int _printf(const char *format, ...)
 
 				len = 0;
 				while (*str != '\0')
+				{
+					count = write(1, str, 1);
 					len++;
-
-				write(1, str, len);
+					str++;
+				}
 				print_count += len;
 			}
 		}
+		format++;
 	}
 	va_end(print_list);
 
